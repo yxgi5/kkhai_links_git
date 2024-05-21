0a1e45662d834bb0bcee42fe5a715a0eb6e0c855
Andreas Zhang <denglitsch@gmail.com>
2024-05-21 08:53:52 +0800

    feat: add scripts

---
+++ scripts/unpack.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+TOP=$PWD
+
+cd ${TOP}/BL.V.0701-0800/
+./extra7z_del_html_url.sh
+cd ${TOP}
+cd ${TOP}/BL.V.0801-0900/
+./extra7z_del_html_url.sh
+cd ${TOP}
+cd ${TOP}/BL.V.0901-1000/
+./extra7z_del_html_url.sh
+cd ${TOP}
+cd ${TOP}/BL.V.1001-1100/
+./extra7z_del_html_url.sh
+cd ${TOP}
+cd ${TOP}/BL.V.1101-1188/
+./extra7z_del_html_url.sh
+cd ${TOP}
+cd ${TOP}/BL.V.1189+/
+./extra7z_del_html_url.sh
+cd ${TOP}
+
+
+

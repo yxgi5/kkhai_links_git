89e7359c74ff2e658070a099775cf986d00fc0b6
Andreas Zhang <denglitsch@gmail.com>
2024-07-03 12:59:55 +0800

    update:

--- count_folder_size_files.sh
+++ count_folder_size_files.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
 for folders in `find . -maxdepth 1 -type d | sed -e 's/^\.$//' | sed -e '/^$/d' | sed -e 's/.\///'`
 do 
 echo -ne `basename ${folders}`
@@ -7,6 +10,9 @@ echo -ne ' ['`find ${folders} -type f | wc -l | sed 's/$/P/'`
 echo -ne '-'`du -sh ${folders} | cut -f1`']'
 echo
 done
+
+IFS=$SAVEIFS
+
 
 # usage:
 # ./count_folder_size_files.sh | tee newnames.txt
